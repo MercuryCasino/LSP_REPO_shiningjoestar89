7c8a6e708d39b6a559b4794d06410678ac635de3
Aqua <Dshawandunwell26@gmail.com>
2025-08-23 18:45:52 -0400

    Second Commit.

--- LSP_Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,7 +1,10 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
-
+	/**
+	 * Dshawan Dunwell
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.print("Hello World!!!");
 
